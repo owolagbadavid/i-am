3ed5acca999a5b863fafe0de5432803cbf7c42b8
David <owolagbadavid@gmail.com>
2025-07-17 19:58:12 +0100

    use int for default 0

--- src/main/java/me/oreos/iam/Dtos/AuthorizationRequestDto.java
+++ src/main/java/me/oreos/iam/Dtos/AuthorizationRequestDto.java
@@ -2,8 +2,7 @@ package me.oreos.iam.Dtos;
 
 public class AuthorizationRequestDto {
     public String authToken;
-    public Integer resourceId;
+    public int resourceId;
     public String action;
     public String resourceType;
 }
-
