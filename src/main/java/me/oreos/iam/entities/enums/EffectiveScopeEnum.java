f9d4eff908b7bdbe0aa55b18bc73b93e1d6c50da
David <owolagbadavid@gmail.com>
2025-07-17 09:35:28 +0100

    simp enum

--- src/main/java/me/oreos/iam/entities/enums/EffectiveScopeEnum.java
+++ src/main/java/me/oreos/iam/entities/enums/EffectiveScopeEnum.java
@@ -1,19 +1,9 @@
 package me.oreos.iam.entities.enums;
 
 public enum EffectiveScopeEnum {
-    OWN("own"),
-    GROUP("group"),
-    ALL("all"),
-    ITEM("item"),
-    DEFAULT("default");
-
-    private String value;
-
-    EffectiveScopeEnum(String value) {
-        this.value = value;
-    }
-
-    public String getValue() {
-        return value;
-    }
+    OWN,
+    GROUP,
+    ALL,
+    ITEM,
+    DEFAULT;
 }
--- src/main/java/me/oreos/iam/entities/enums/EnforcementScopeEnum.java
+++ src/main/java/me/oreos/iam/entities/enums/EnforcementScopeEnum.java
@@ -1,17 +1,7 @@
 package me.oreos.iam.entities.enums;
 
 public enum EnforcementScopeEnum {
-    OWN("own"),
-    GROUP("group"),
-    DEFAULT("default");
-
-    private String value;
-
-    EnforcementScopeEnum(String value) {
-        this.value = value;
-    }
-
-    public String getValue() {
-        return value;
-    }
+    OWN,
+    GROUP,
+    DEFAULT;
 }
